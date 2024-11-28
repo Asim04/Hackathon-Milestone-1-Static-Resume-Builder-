8e2e7e3199c04f3c9f678cf01e4c126e182d689a
Muhammad asim kahn <asimkhanr1@gmail.com>
2024-11-29 02:49:21 +0500

    Deleted past commit

---
+++ script.ts
@@ -0,0 +1,26 @@
+
+// ------------Refrence Section--------------------
+let ref_section = document.getElementById("ref")
+let ref_button = document.getElementById("ref_button")
+
+
+ref_button?.addEventListener("click", ()=>{
+    // ref_section! .style.display = "none"
+    ref_section! .classList.toggle("hide")
+})
+
+// ------------experince Section--------------------
+let experience = document.getElementById("Experience")
+let experience_button = document.getElementById("experience_button")
+
+experience_button! .addEventListener("click", ()=>{
+    experience!.classList.toggle("hide")
+
+})
+
+// print Button
+let print_Button = document.getElementById("print_button")
+
+print_Button!.addEventListener("click", ()=>{
+    window.print()
+})
